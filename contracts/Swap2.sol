64a1a73654a2666376750622d0dec23831c3ef67
Levan <swiftcoder@MacBook-Pro-Levan.local>
2022-07-14 23:53:19 +0400

    fix naming

--- contracts/Swap2.sol
+++ contracts/Swap2.sol
@@ -13,8 +13,8 @@ contract NFTSwap2 {
     Counters.Counter private _swapId;
 
     struct Swap {
-        address from;
-        address to;
+        address user1;
+        address user2;
         address contract1;
         address contract2;
         uint256 tokenId1;
@@ -26,13 +26,13 @@ contract NFTSwap2 {
     mapping(uint256 => Swap) swaps;
 
     function createSwap(
-        address to,
+        address user2,
         address contract1,
         address contract2,
         uint256 tokenId1,
         uint256 tokenId2
     ) public returns (uint256){
-        require(to != address(0x0), "to address cant be zero");
+        require(user2 != address(0x0), "to address cant be zero");
         require(contract1 != address(0x0), "contract1 address cant be zero");
         require(contract2 != address(0x0), "contract2 address cant be zero");
 
@@ -41,7 +41,7 @@ contract NFTSwap2 {
 
         swaps[swapId] = Swap(
             msg.sender,
-            to,
+            user2,
             contract1,
             contract2,
             tokenId1,
@@ -58,11 +58,11 @@ contract NFTSwap2 {
         require(IERC721(contractAddress).isApprovedForAll(msg.sender, address(this)), "deposit: need approve your nft usage");
 
         Swap storage current = swaps[swapId];
-        require(current.from != address(0x0), "swap not exist");
+        require(current.user1 != address(0x0), "swap not exist");
 
-        if (current.from == msg.sender) {
+        if (current.user1 == msg.sender) {
             current.token1Received = true;
-        } else if (current.to == msg.sender) {
+        } else if (current.user2 == msg.sender) {
             current.token2Received = true;
         }
 
@@ -73,14 +73,14 @@ contract NFTSwap2 {
         require(swapId <= _swapId._value, "swapId less than maximum _swapId");
 
         Swap memory current = swaps[swapId];
-        require(current.from != address(0x0), "swap not exist");
+        require(current.user1 != address(0x0), "swap not exist");
 
         if (current.token1Received == true) {
-            IERC721(current.contract1).safeTransferFrom(address(this), current.from, current.tokenId1);
+            IERC721(current.contract1).safeTransferFrom(address(this), current.user1, current.tokenId1);
         }
 
         if (current.token2Received == true) {
-            IERC721(current.contract2).safeTransferFrom(address(this), current.to, current.tokenId2);
+            IERC721(current.contract2).safeTransferFrom(address(this), current.user2, current.tokenId2);
         }
 
         delete swaps[swapId];
@@ -90,11 +90,11 @@ contract NFTSwap2 {
         require(swapId <= _swapId._value, "swapId less than maximum _swapId");
 
         Swap memory current = swaps[swapId];
-        require(current.from != address(0x0), "swap not exist");
+        require(current.user1 != address(0x0), "swap not exist");
         require(current.token1Received && current.token2Received, "terms of the deal were not met");
 
-        IERC721(current.contract1).safeTransferFrom(address(this), current.to, current.tokenId1);
-        IERC721(current.contract2).safeTransferFrom(address(this), current.from, current.tokenId2);
+        IERC721(current.contract1).safeTransferFrom(address(this), current.user2, current.tokenId1);
+        IERC721(current.contract2).safeTransferFrom(address(this), current.user1, current.tokenId2);
 
         delete swaps[swapId];
     }
